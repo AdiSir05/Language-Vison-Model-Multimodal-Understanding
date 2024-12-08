7acb4852744298047d122bb8d7787875b2149184
Varun Chitturi <varunchitturi@icloud.com>
2024-12-07 23:32:45 -0500

    finish extension

--- score.py
+++ score.py
@@ -45,5 +45,6 @@ def f1(tp, tn, fp, fn):
     return {
         "Precision": tp/(tp+fp),
         "Recall": tp/(tp+fn),
-        "F1-score": 2*tp/(2*tp+fp+fn)
+        "F1-score": 2*tp/(2*tp+fp+fn),
+        "Accuracy": (tp + tn)/(tp + tn + fp + fn)
     }
