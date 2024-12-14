0feef4098eb9b4910e6b9468bf540d066c65d23d
Varun Chitturi <varunchitturi@icloud.com>
2024-12-14 14:02:26 -0500

    finish

--- score.py
+++
@@ -1,7 +0,0 @@
-def f1(tp, tn, fp, fn):
-    return {
-        "Precision": tp/(tp+fp),
-        "Recall": tp/(tp+fn),
-        "F1-score": 2*tp/(2*tp+fp+fn),
-        "Accuracy": (tp + tn)/(tp + tn + fp + fn)
-    }
